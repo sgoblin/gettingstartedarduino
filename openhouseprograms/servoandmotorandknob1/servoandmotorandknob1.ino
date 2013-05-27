a79d9095ca410767316d28778c4e541a61894b3f
sgoblin <rsuper@sonic.net>
2013-05-26 17:14:55 -0700

    The program runs almost perfectly now on an arduino uno from the Sparkfun Inventor's Kit.  This will probably be the final release.

--- openhouseprograms/servoandmotorandknob1/servoandmotorandknob1.ino
+++ openhouseprograms/servoandmotorandknob1/servoandmotorandknob1.ino
@@ -31,45 +31,50 @@ void setup()
   
   //This sets up a serial port so thst you can control it from your computer
   Serial.begin(9600);
-  
- // pinMode(knobPin, INPUT);
-  
-  analogWrite(motorPin, 254);
-  delay(100);
-  analogWrite(motorPin, 65);
 }
 
 void loop()
 {
+  //Finds the value that the knob is giving
+  knobVal = analogRead(knobPin);
   //Runs the servomove function
-//  servomove();
+  servomove(knobVal);
   
   //Runs the motorSpeed function
-  motorSpeed();
+  motorSpeed(knobVal);
 }
 
-void servomove()
+void servomove(int knobval)
 {
   //This code is from an arduino example that comes with the arduino SDK.
-  //Thanks to Michal Rinott for this code.
-  knobVal = analogRead(knobPin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  knobVal = map(knobVal, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 1 and 180) 
-  myservo.write(knobVal);                  // sets the servo position according to the scaled value 
+  //Thanks to Michal Rinott for some of this code.
+  knobval = map(knobval, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 1 and 180) 
+  myservo.write(knobval);                  // sets the servo position according to the scaled value   
   delay(15);                           // waits for the servo to get there (the number is in milliseconds)
 }
 
-void motorSpeed()
+void motorSpeed(int knobval)
 {
   //This code was inspired by and based off of a Sparkfun Inventor's Kit project.
   //Sparkfun released this code into the public domain.
-  int speedstart = analogRead(knobPin);
+  //NOTE: The code that is greyed out here is the code that would send a speed to the motor.
+  //int speedstart = analogRead(knobPin);
   ////This changing of knobVal doesn't affect the one in servomove because it is within a function.
-  int speed1 = map(speedstart, 0, 1023, 65, 254);  //This scales the value from the knob(0-1023) to a range between 0 and 255.
+  //int speed1 = map(speedstart, 65, (1023+65), 65, 255);  //This scales the value from the knob(0-1023) to a range between 0 and 255.
   //That is the range of speeds for the motor.
   //This puts the value (0-255) into the motor, and the speed changes accordingly.
-  analogWrite(motorPin, speed1);
+  //analogWrite(motorPin, speed1);
   //Just a small delay to smooth things out.
- // delay(15);
+  //delay(15);
+  
+  //The value coming from the knob can be from 1-1024
+  //This just checks if it's below or equal to 512.
+  //If it is, then it turns the motor off.
+  if (knobval <= 512){
+    digitalWrite(motorPin, LOW);//If it is anything else, it turns the motor on.
+  }else{
+    digitalWrite(motorPin, HIGH);
+  }
 }
   
  
